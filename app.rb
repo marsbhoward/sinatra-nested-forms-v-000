99c7b0eb66500bf6ebb935a0cb6f77638a2df85b
Mars Howard <marsbhoward@gmail.com>
2017-11-19 05:18:55 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,25 +4,25 @@ require_relative './models/pirate.rb'
 require_relative './models/ship.rb'
 
 module FormsLab
-    class App < Sinatra::Base
+  class App < Sinatra::Base
 
-      # code other routes/actions here
-     get "/" do
-       erb :root
-     end
-
-     get "/new" do
-       erb :'pirates/new'
-     end
-
-     post "/pirates" do
-       @pirate = Pirate.new(params[:pirate])
-
-
-       @ships = Ship.all
-
-       erb :'views/pirates/show'
-     end
-
+    # code other routes/actions here
+    get "/" do
+      erb :root
     end
+
+    get "/new" do
+      erb :'pirates/new'
+    end
+
+    post "/pirates" do
+      @pirate = Pirate.new(params[:pirate])
+
+
+      @ships = Ship.all
+
+      erb :'pirates/show'
+    end
+
   end
+end
