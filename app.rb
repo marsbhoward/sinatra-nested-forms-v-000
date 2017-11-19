54b16465057dedf32c88b6c5d05e7e41b061a691
Mars Howard <marsbhoward@gmail.com>
2017-11-19 05:03:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,22 +5,20 @@ module FormsLab
 
     # code other routes/actions here
     get "/" do
-         erb :root
-       end
-
-       get "/new" do
-         erb :'pirates/new'
-       end
-
-       post '/pirates' do
-        @pirate = Pirate.new(params[:pirate])
-
-        params[:pirate][:ships].each do |details|
-          Ship.new(details)
-        end
-        @ships = Ship.all
-
-        erb :'pirates/show'
-       end
+       erb :root
+    end
+ 
+    get "/new" do
+      erb :'pirates/new'
+    end
+ 
+    post "/pirates" do
+       @pirate = Pirate.new(params[:pirate])
+ 
+ 
+       @ships = Ship.all
+ 
+       erb :'pirates/show'
+   end
   end
 end
