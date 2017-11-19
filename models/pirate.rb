6acd71c65006f9a336272768329e6ccbf70d9a20
Mars Howard <marsbhoward@gmail.com>
2017-11-19 05:06:54 +0000

    automatically backed up by learn

--- models/pirate.rb
+++ models/pirate.rb
@@ -1,13 +1,16 @@
 class Pirate
   attr_reader :name, :weight, :height
 
-  PIRATES = []
+  @@all = []
 
   def initialize(args)
     @name = args[:name]
     @weight = args[:weight]
     @height = args[:height]
-    PIRATES << self
+    @@all << self
+    params[:ships].each do |ship|
+      Ship.new(ship)
+    end
   end
 
   def self.all
