3ca8a39b22ddbab9d1e4db28b880c74eb393c553
Mars Howard <marsbhoward@gmail.com>
2017-11-19 05:04:06 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,17 +7,17 @@ module FormsLab
     get "/" do
        erb :root
     end
- 
+
     get "/new" do
       erb :'pirates/new'
     end
- 
+
     post "/pirates" do
        @pirate = Pirate.new(params[:pirate])
- 
- 
+
+
        @ships = Ship.all
- 
+
        erb :'pirates/show'
    end
   end
