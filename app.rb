f021fb45c180cc4c63049f2fd89e11ee1de43f5a
Mars Howard <marsbhoward@gmail.com>
2017-11-19 04:40:04 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,6 +4,21 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get "/" do
+         erb :root
+       end
 
+       get "/new" do
+         erb :'pirates/new'
+       end
+
+       post "/pirates" do
+         @pirate = Pirate.new(params[:pirate])
+
+
+         @ships = Ship.all
+
+         erb :'pirates/show'
+       end
   end
 end
